0b35ef35b5cfab1a61dc382986f220c3ea5ef6c2
Jleung123 <57479120+Jleung123@users.noreply.github.com>
2019-11-28 03:02:05 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,8 @@
+		flag=getCookie("flag");
+		if(flag =="true")
+		{
+			document.writeln("<td width='100' align=center><a href='logoff.html'>Log Off</a></td>");
+		}
+		else
+			document.writeln("<td width='100' align=center><a href='login.html'>Log In</a></td>");
+	
